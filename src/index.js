d6161ee49044560aca198fd7e684e30706417970
prasanna22-optisol <prasannakumar.m@optisolbusiness.com>
2025-01-23 15:58:04 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import {connectDB} from "./database/db.js";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/authRoute.js";
 import userRouter from "./routes/userRoute.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -23,6 +24,10 @@ app.use(cookieParser())
 app.use(express.urlencoded({ extended:true}))
 
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}))
 app.use("/api/auth",authRouter)
 app.use("/api/users",userRouter)
 
